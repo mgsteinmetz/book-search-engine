29a0c3d9e373031776bd3640d286ed45eda3cd95
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-18 13:43:07 -0500

    filling in gql for typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,23 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        name: String!
+    }
+
+    type Book {
+        _id: ID!
+    }
+
+    type Query {
+        user: [User]
+        book(_id: String): [Book]
+    }
+
+    type Mutation {
+        createUser(_id: String!): [User]
+    }
+`;
 
 module.exports = typeDefs;
