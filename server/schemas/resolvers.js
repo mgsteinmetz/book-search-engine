f05e1b67ecaeb7c5906400c398ebe005cf4ebff0
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-18 20:59:23 -0500

    finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,21 @@
 const { Book, User } = require('../models');
 
-const resolvers = {};
+const resolvers = {
+    Query: {
+        user: async () => {
+            return User.find({});
+        },
+        book: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return Book.find(params);
+        },
+    },
+    Mutation: {
+        createUser: async (parent, args) => {
+            const user = await User.create(args);
+            return user;
+        }
+    }
+};
 
 module.exports = resolvers;
