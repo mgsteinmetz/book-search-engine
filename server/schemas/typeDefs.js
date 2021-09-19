7146404482d1a18b1e0aaeb4fe8e15877e87908e
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-18 20:39:51 -0500

    finishing typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,11 +3,19 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
     type User {
         _id: ID!
-        name: String!
+        username: String! @unique
+        email: String! @unique
+        password: String!
+        savedBooks: [bookSchema]
     }
 
     type Book {
-        _id: ID!
+        authors: [String]
+        desciption: String!
+        bookId: ID!
+        image: String
+        link: String
+        title: String!
     }
 
     type Query {
