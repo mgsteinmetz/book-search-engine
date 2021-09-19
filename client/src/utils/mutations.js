9c31aa8be415f2fcf688b53382255db6249a3418
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-19 14:12:41 -0500

    finishing mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,57 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-    mutation createUser($username: String!, $email: String!, $password: String!, savedBooks: $[bookSchema]) {
-        createUser(username: $username, email: $email, password: $password, $savedbooks: $[bookSchema]) {
+export const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookId: String!) {
+        saveBook(bookId: $bookId) {
             username
-            email
-            password
-            savedBooks
+            savedBooks {
+                bookId
+                title
+                authors
+                image
+                description
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            username
+            savedBooks {
+                bookId
+                title
+                authors
+                image
+                description
+                link
+            }
         }
     }
 `;
