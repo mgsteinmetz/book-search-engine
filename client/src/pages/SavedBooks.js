15fe2005cd4565b11e340fea0d254c1ab0f05d03
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-09-20 16:28:50 -0500

    still not working whyyyy

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
@@ -15,7 +14,7 @@ const SavedBooks = () => {
 
   const { load, data } = useQuery(GET_ME);
   const userData = data?.me || {};
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook, { error }] = UseMutation(REMOVE_BOOK);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
